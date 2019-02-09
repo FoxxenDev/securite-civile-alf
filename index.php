b997d85f7074cf57fd4a5638796f62751be25239
FoxxenDev <32935159+FoxxenDev@users.noreply.github.com>
2019-02-09 23:08:40 +0100

    Home page modify

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php require("inc/header.php"); ?>
-Bienvenue sur le site de la Sécurité Civile. Ce site à pour but D'organiser les informations essentielles (surtout de viré les dizaine de googles docs différents). Si vous rencontrez un problème avec le site contactez Taylor Williams.
+    <h3>Bienvenue sur le site de la Sécurité Civile.</h3>
+    <p>Ce site à pour but d'organiser les informations essentielles.</p>
 <?php require("inc/footer.php"); ?>
