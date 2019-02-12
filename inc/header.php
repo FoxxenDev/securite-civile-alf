610106234ebe5ebb21239acd065f1aeb9536362c
FoxxenDev <32935159+FoxxenDev@users.noreply.github.com>
2019-02-12 22:56:33 +0100

    Reconnexion automatique

--- inc/header.php
+++ inc/header.php
@@ -137,14 +137,14 @@ desired effect
 							<!-- Menu Toggle Button -->
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 								<!-- The user image in the navbar-->
-								<img src="/secuv2/dist/img/avatar5.png" class="user-image" alt="User Image">
+								<img src="/secuv2/dist/img/avatar<?= $_SESSION["auth"]->avatar ?>.png" class="user-image" alt="User Image">
 								<!-- hidden-xs hides the username on small devices so only the image appears. -->
 								<span class="hidden-xs"><?= $_SESSION["auth"]->username ?></span>
 							</a>
 							<ul class="dropdown-menu">
 								<!-- The user image in the menu -->
 								<li class="user-header">
-									<img src="/secuv2/dist/img/avatar5.png" class="img-circle" alt="User Image">
+									<img src="/secuv2/dist/img/avatar<?= $_SESSION["auth"]->avatar ?>.png" class="img-circle" alt="User Image">
 
 									<p>
 										<?= $_SESSION["auth"]->username ?> - <?= $_SESSION["auth"]->rank_sc_name ?>
@@ -189,7 +189,7 @@ desired effect
 				<!-- Sidebar user panel (optional) -->
 				<div class="user-panel">
 					<div class="pull-left image">
-						<img src="/secuv2/dist/img/avatar5.png" class="img-circle" alt="User Image">
+						<img src="/secuv2/dist/img/avatar<?= $_SESSION["auth"]->avatar ?>.png" class="img-circle" alt="User Image">
 					</div>
 					<div class="pull-left info">
 						<p><?= $_SESSION["auth"]->username ?></p>
